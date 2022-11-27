e7624326acc2789065486fb7bb4423cb3dea7c3b
mariferreras <marinaferreras@icloud.com>
2022-11-27 22:55:03 +0100

    guardadomari

--- SQL/pairprogramming_consultas_avanzadas2.sql
+++ SQL/pairprogramming_consultas_avanzadas2.sql
@@ -1,4 +1,6 @@
-# Relación entre número de pedidos y máxima carga:
+USE sys;
+
+#1 Relación entre número de pedidos y máxima carga:
 # Desde logística nos piden el número de pedidos y la máxima cantidad de carga de entre los mismos
 # (freight) que han sido enviados por cada empleado (mostrando el ID de empleado en cada caso).
 
@@ -7,7 +9,7 @@ FROM orders
 WHERE freight >= 100
 GROUP BY employee_id, freight;
 
-# Descartar pedidos sin fecha y ordénalos:
+#2 Descartar pedidos sin fecha y ordénalos:
 # Una vez han revisado los datos de la consulta anterior, nos han pedido afinar un poco más el "disparo". En
 # el resultado anterior se han incluido muchos pedidos cuya fecha de envío estaba vacía, por lo que 
 # tenemos que mejorar la consulta en este aspecto. También nos piden que ordenemos los resultados
@@ -19,29 +21,31 @@ WHERE shipped_date
 GROUP BY employee_id, freight, shipped_date
 ORDER BY employee_id;
 
-# Números de pedidos por día:
+#3 Números de pedidos por día:
 # El siguiente paso en el análisis de los pedidos va a consistir en conocer mejor la distribución de los 
 # mismos según las fechas. Por lo tanto, tendremos que generar una consulta que nos saque el número de
 # pedidos para cada día, mostrando de manera separada el día (DAY()), el mes (MONTH()) y el año 
 # (YEAR()).
 
-SELECT MAX(shipped_date) AS shipped_date, order_id AS order_id, employee_id AS employee_id
+SELECT COUNT(DISTINCT order_id) AS NumeroPedido, DAY(order_date) AS Día, MONTH(order_date) AS Mes, YEAR(order_date) AS Año
 FROM orders
-GROUP BY shipped_date, order_id, employee_id
-ORDER BY
-DAY(shipped_date), MONTH(shipped_date), YEAR(shipped_date);
+GROUP BY DAY(order_date), MONTH(order_date), YEAR(order_date);
 
 
-# Número de pedidos por mes y año:
+#4 Número de pedidos por mes y año:
 # La consulta anterior nos muestra el número de pedidos para cada día concreto, pero esto es demasiado 
 # detalle. Genera una modificación de la consulta anterior para que agrupe los pedidos por cada mes 
 # concreto de cada año.
 
-# Seleccionad las ciudades con 4 o más empleadas:
+SELECT COUNT(DISTINCT order_id) AS NumeroPedido, MONTH(order_date) AS Mes, YEAR(order_date) AS Año
+FROM orders
+GROUP BY MONTH(order_date), YEAR(order_date) ORDER BY YEAR(order_date);
+
+#5 Seleccionad las ciudades con 4 o más empleadas:
 # Desde recursos humanos nos piden seleccionar los nombres de las ciudades con 4 o más empleadas de 
 # cara a estudiar la apertura de nuevas oficinas.
 
-SELECT order_id, unit_price,
+SELECT order_id AS OrderID, unit_price AS PrecioUnidad,
 CASE
 	WHEN unit_price > 2000 THEN "Alto"
     ELSE "Bajo"
@@ -49,10 +53,13 @@ CASE
 FROM order_details;
 
 
-# Cread una nueva columna basándonos en la cantidad monetaria:
+#6 Cread una nueva columna basándonos en la cantidad monetaria:
 # Necesitamos una consulta que clasifique los pedidos en dos categorías ("Alto" y "Bajo") en función de la
 # cantidad monetaria total que han supuesto: por encima o por debajo de 2000 euros.
 
-Pedidos por empresa en UK:
-
-SELECT
+SELECT order_id as NumeroPedido, ROUND(unit_price * quantity, 1) AS Total,
+CASE 
+WHEN unit_price * quantity > 2000 THEN "Alto"
+ELSE "Bajo"
+END AS Categoria
+FROM order_details;
